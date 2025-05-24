faeb03ce7c1d3151300e030cdcd2dc6c663b0850
Bikram Kumar <coolbikram0@gmail.com>
2025-05-24 13:01:08 +0530

    ppu register organization

--- GalaxyShooter.asm
+++ GalaxyShooter.asm
@@ -5,6 +5,18 @@
   .inesmap 0   ; mapper 0 = NROM, no bank swapping
   .inesmir 1   ; background mirroring
   
+  
+; --- PPU Registers ---
+
+PPU_CTRL  = $2000  ; VPHB SINN
+PPU_MASK  = $2001  ; BGRs bMmG
+PPU_STATUS = $2002
+OAM_ADDR  = $2003
+OAM_DATA  = $2004
+PPU_SCROLL = $2005
+PPU_ADDR  = $2006
+PPU_DATA  = $2007
+OAM_DMA  = $4014
 
 ;;;;;;;;;;;;;;;
 
@@ -19,12 +31,12 @@ RESET:
   LDX #$FF
   TXS          ; Set up stack
   INX          ; now X = 0
-  STX $2000    ; disable NMI
-  STX $2001    ; disable rendering
+  STX PPU_CTRL    ; disable NMI
+  STX PPU_MASK    ; disable rendering
   STX $4010    ; disable DMC IRQs
 
 vblankwait1:       ; First wait for vblank to make sure PPU is ready
-  BIT $2002
+  BIT PPU_STATUS
   BPL vblankwait1
 
 clrmem:
@@ -42,23 +54,19 @@ clrmem:
   BNE clrmem
    
 vblankwait2:      ; Second wait for vblank, PPU is ready after this
-  BIT $2002
+  BIT PPU_STATUS
   BPL vblankwait2
 
 
 LoadPalettes:
-  LDA $2002             ; read PPU status to reset the high/low latch
+  LDA PPU_STATUS             ; read PPU status to reset the high/low latch
   LDA #$3F
-  STA $2006             ; write the high byte of $3F00 address
+  STA PPU_ADDR             ; write the high byte of $3F00 address
   LDA #$00
-  STA $2006             ; write the low byte of $3F00 address
+  STA PPU_ADDR             ; write the low byte of $3F00 address
   LDX #$00              ; start out at 0
 LoadPalettesLoop:
-  LDA palette, x        ; load data from address (palette + the value in x)
-                          ; 1st time through loop it will load palette+0
-                          ; 2nd time through loop it will load palette+1
-                          ; 3rd time through loop it will load palette+2
-                          ; etc
+  LDA palette, x        
   STA $2007             ; write to PPU
   INX                   ; X = X + 1
   CPX #$20              ; Compare X to hex $10, decimal 16 - copying 16 bytes = 4 sprites
@@ -80,11 +88,11 @@ LoadSpritesLoop:
               
               
 LoadBackground:
-  LDA $2002             ; read PPU status to reset the high/low latch
+  LDA PPU_STATUS             ; read PPU status to reset the high/low latch
   LDA #$20
-  STA $2006             ; write the high byte of $2000 address
+  STA PPU_ADDR             ; write the high byte of $2000 address
   LDA #$00
-  STA $2006             ; write the low byte of $2000 address
+  STA PPU_ADDR             ; write the low byte of $2000 address
   LDX #$00              ; start out at 0
 LoadBackgroundLoop:
   LDA background, x     ; load data from address (background + the value in x)
@@ -96,11 +104,11 @@ LoadBackgroundLoop:
               
               
 LoadAttribute:
-  LDA $2002             ; read PPU status to reset the high/low latch
+  LDA PPU_STATUS             ; read PPU status to reset the high/low latch
   LDA #$23
-  STA $2006             ; write the high byte of $23C0 address
+  STA PPU_ADDR             ; write the high byte of $23C0 address
   LDA #$C0
-  STA $2006             ; write the low byte of $23C0 address
+  STA PPU_ADDR             ; write the low byte of $23C0 address
   LDX #$00              ; start out at 0
 LoadAttributeLoop:
   LDA attribute, x      ; load data from address (attribute + the value in x)
@@ -115,10 +123,10 @@ LoadAttributeLoop:
               
               
   LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
-  STA $2000
+  STA PPU_CTRL
 
   LDA #%00011110   ; enable sprites, enable background, no clipping on left side
-  STA $2001
+  STA PPU_MASK
 
 Forever:
   JMP Forever     ;jump back to Forever, infinite loop
@@ -127,9 +135,9 @@ Forever:
 
 NMI:
   LDA #$00
-  STA $2003       ; set the low byte (00) of the RAM address
+  STA OAM_ADDR       ; set the low byte (00) of the RAM address
   LDA #$02
-  STA $4014       ; set the high byte (02) of the RAM address, start the transfer
+  STA OAM_DMA       ; set the high byte (02) of the RAM address, start the transfer
 
 
 LatchController:
@@ -146,7 +154,7 @@ ReadA:
                   ; add instructions here to do something when button IS pressed (1)
   LDA $0203       ; load sprite X position
   CLC             ; make sure the carry flag is clear
-  ADC #$01        ; A = A + 1
+  ADC #$05        ; A = A + 1
   STA $0203       ; save sprite X position
 ReadADone:        ; handling this button is done
   
@@ -162,15 +170,14 @@ ReadB:
   STA $0203       ; save sprite X position
 ReadBDone:        ; handling this button is done
 
-
-  ;;This is the PPU clean up section, so rendering the next frame starts properly.
+PPU_Cleanup:
   LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
-  STA $2000
+  STA PPU_CTRL
   LDA #%00011110   ; enable sprites, enable background, no clipping on left side
-  STA $2001
+  STA PPU_MASK
   LDA #$00        ;;tell the ppu there is no background scrolling
-  STA $2005
-  STA $2005
+  STA PPU_SCROLL
+  STA PPU_SCROLL
   
   RTI             ; return from interrupt
  
@@ -181,15 +188,15 @@ ReadBDone:        ; handling this button is done
   .bank 1
   .org $E000
 palette:
-  .db $22,$29,$1A,$0F,  $22,$36,$17,$0F,  $22,$30,$21,$0F,  $22,$27,$17,$0F   ;;background palette
-  .db $22,$1C,$15,$14,  $22,$02,$38,$3C,  $22,$1C,$15,$14,  $22,$02,$38,$3C   ;;sprite palette
+  .db $1F,$2A,$28,$1C,  $1F,$36,$17,$0F,  $1F,$30,$21,$0F,  $1F,$27,$17,$0F   ;;background palette
+  .db $1F,$2A,$28,$1C,  $1F,$02,$38,$3C,  $1F,$1C,$15,$14,  $1F,$02,$38,$3C   ;;sprite palette
 
 sprites:
      ;vert tile attr horiz
-  .db $80, $32, $00, $80   ;sprite 0
-  .db $80, $33, $00, $88   ;sprite 1
-  .db $88, $34, $00, $80   ;sprite 2
-  .db $88, $35, $00, $88   ;sprite 3
+  .db $80, $01, $00, $80   ;sprite 0
+  .db $80, $02, $00, $88   ;sprite 1
+  .db $88, $11, $00, $80   ;sprite 2
+  .db $88, $12, $00, $88   ;sprite 3
 
 
 background:
@@ -197,10 +204,10 @@ background:
   .db $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24  ;;all sky
 
   .db $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24  ;;row 2
-  .db $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24  ;;all sky
+  .db $24,$24,$24,$00,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24  ;;all sky
 
-  .db $24,$24,$24,$24,$45,$45,$24,$24,$45,$45,$45,$45,$45,$45,$24,$24  ;;row 3
-  .db $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$53,$54,$24,$24  ;;some brick tops
+  .db $24,$24,$24,$24,$00,$03,$04,$24,$45,$45,$45,$45,$45,$45,$24,$24  ;;row 3
+  .db $24,$24,$24,$24,$24,$13,$14,$24,$24,$24,$24,$24,$53,$54,$24,$24  ;;some brick tops
 
   .db $24,$24,$24,$24,$47,$47,$24,$24,$47,$47,$47,$47,$47,$47,$24,$24  ;;row 4
   .db $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$55,$56,$24,$24  ;;brick bottoms
